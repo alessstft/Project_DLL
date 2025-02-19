25f25717470ebf27bb4468ea2f20b24d377496d3
Катя <katya.kotel@bk.ru>
2025-02-19 10:39:20 +0300

    fjfjvf

--- Proect_dll/Header.cpp
+++ Proect_dll/Header.cpp
@@ -5,18 +5,28 @@
 #include <windows.h>
 
 using namespace std;
+
 static string path_;
 static ofstream out_;
 
 void open_file(const char* path) {
     path_ = string(path);
-    out_ = ofstream out(path_, ios::app);
+    out_.open(path_, ios::app);
+    
+    if (!out_) {
+        cerr << "Error: Unable to open file " << path_ << endl;
+    }
 }
 
 void close_file() {
-    out_.close();
+    if (out_.is_open()) {
+        out_.close();
+    }
 }
 
 void save() {
-    out_.close();
+    if (out_.is_open()) {
+        out_.flush();  // Ensures data is written before closing
+        out_.close();
+    }
 }
