ba42306f5993b5df68031fc162ba89c12fe37c37
madagaskarvb <149654664+madagaskarvb@users.noreply.github.com>
2025-02-24 10:16:09 +0300

    Update Header.h

--- Proect_dll/Header.h
+++ Proect_dll/Header.h
@@ -3,7 +3,7 @@
 #define HEADER_H
 
 #ifdef EXPORTING_DLL
-#define DLL_API __declspec(dllexport)  // Для экспорта функций
+#define DLL_API __declspec(dllexport)  // Г„Г«Гї ГЅГЄГ±ГЇГ®Г°ГІГ  ГґГіГ­ГЄГ¶ГЁГ©
 #else
 #define DLL_API __declspec(dllimport)
 #endif
@@ -12,6 +12,9 @@ extern "C" {
     DLL_API void open_file(const char* path);
     DLL_API void close_file();
     DLL_API void save();
+    DLL_API void delete_file();
+    DLL_API void count_words();
+    DLL_API void write();
 }
 
 #endif
