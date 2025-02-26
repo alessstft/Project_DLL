88350bd7aee26ee580d5b158e6abd3b68e1a2551
madagaskarvb <149654664+madagaskarvb@users.noreply.github.com>
2025-02-26 13:22:19 +0300

    Update Header.cpp

--- Proect_dll/Header.cpp
+++ Proect_dll/Header.cpp
@@ -62,3 +62,50 @@ void write(const char* path, const char* str) {
     file.close();
 }
 
+const char* read_file(const char* path) {
+    ifstream file;
+    file.open(path);
+    if (!file.is_open()) {
+        return "Error!";
+    }
+    string line;
+    char* content = new char[0] {};
+    while (getline(file, line)) {
+        strcpy_s(content, strlen(content) + line.size(), line.c_str());
+    }
+    file.close();
+
+    return content;
+}
+
+
+bool search_in_file(const char* path, const char* thing) {
+    ifstream file;
+    string line, text, thing_str;
+
+    file.open(path);
+    while (getline(file, line)) {
+        text += line + '\n';
+    }
+    file.close();
+    thing_str.assign(thing, strlen(thing));
+    
+    return text.find(thing) != string::npos;
+}
+
+
+size_t get_file_size(const char* path)
+{
+    cerr << "Attempting to get file size for: " << path << endl;
+    ifstream in(path, ios::binary | ios::ate);
+    if (!in) {
+        cerr << "Error: Unable to open file " << path << " to determine size." << endl;
+        return 0;
+    }
+
+    size_t size = static_cast<size_t>(in.tellg());
+    in.close();
+
+    cerr << "File size is " << size << " bytes." << endl;
+    return size;
+}
